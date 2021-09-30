321d89fcc09d97ea4794f72246803997673791a9
Agris <agrisskesters@gmail.com>
2021-09-30 13:53:48 +0300

    Home work 2

---
+++ homework_23.09.2021.py
@@ -0,0 +1,56 @@
+#1.
+
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+
+print(str(number_1)+" + "+str(number_2)+" = "+str(number_1+number_2))
+print(str(number_1)+" - "+str(number_2)+" = "+str(number_1-number_2))
+print(str(number_1)+" / "+str(number_2)+" = "+str(number_1/number_2))
+print(str(number_1)+" * "+str(number_2)+" = "+str(number_1*number_2))
+
+#2.
+
+phoneNumbers = {
+    "Johny": 37124789011,
+    "Ana": 37126025682,
+    "Jonathan": 37125213377,
+    "Rebeca": 37122996351
+}
+print() # empty line
+print(phoneNumbers)
+
+phoneNumbers["Johny"] = "Jonhy@gmail.com"
+print(phoneNumbers)
+
+phoneNumbers["Kristopher"] = 37123236499
+print(phoneNumbers)
+
+del phoneNumbers["Rebeca"]
+print(phoneNumbers)
+
+#3.
+
+shoppingList = ["bread", "milk", "water", "milk", "milk", "butter", "milk", "bread"]
+correctedShoppingList = list(dict.fromkeys(shoppingList))
+print() # empty line
+print(shoppingList)
+print(correctedShoppingList)
+
+#4.
+
+print() # empty line
+a = int(input("Enter first number: "))
+b = int(input("Enter first number: "))
+
+print("Is "+str(a)+" bigger then "+str(b)+" ?")
+
+if a > b:
+    print(True)
+else:
+    print(False)
+
+
+
+
+
+
