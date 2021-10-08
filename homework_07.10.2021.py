e2562132230f14dab5690ed6b99b6d3ce957b0c2
Agris <agrisskesters@gmail.com>
2021-10-08 20:33:11 +0300

    Home work 4.1

--- homework_07.10.2021.py
+++ homework_07.10.2021.py
@@ -1,30 +1,21 @@
+import random
 playerOption = 0
-wordList = []
+wordList = ["apple"]
 
 while playerOption != 3:
     print("Enter 1 to play hangman, 2 to add a new word, 3 to quit program")
-    playerOption = int(input("Enter number: "))
+    playerOption = input("Enter number: ")
+    randomWord = random.choice(wordList)
+    linesForPrinting = ["-"] * len(randomWord)
 
-    if playerOption == 1:
-        cycleCounter = 3
+    if int(playerOption) == 1:
+        guessingWord = randomWord
+        guessedLetterList = []
+        cycleCounter = 0
 
         while cycleCounter != 5:
 
-            print(listOFPictures[cycleCounter])
-            playerLetterInput = input("Enter letter: ")
-
-    elif playerOption == 2:
-        playerAddWord = input("Enter new word: ")
-        wordList.append(playerAddWord)
-        print("Added "+playerAddWord)
-
-    elif playerOption == 3:
-        break
-
-    else:
-        print("Wrong input!")
-
-listOFPictures = ["+---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+            listOFPictures = ["+---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
                               "+---+\n  |   |\n  o   |\n      |\n      |\n      |\n=========",
                               "+---+\n  |   |\n  o   |\n  |   |\n      |\n      |\n=========",
                               "+---+\n  |   |\n  o   |\n /|   |\n      |\n      |\n=========",
@@ -32,4 +23,34 @@ listOFPictures = ["+---+\n  |   |\n      |\n      |\n      |\n      |\n=========
                               "+---+\n  |   |\n  o   |\n /|\  |\n      |\n /    |\n=========",
                               "+---+\n  |   |\n  o   |\n /|\  |\n      |\n /\   |\n=========",
                               ]
+            print(listOFPictures[cycleCounter])
+            print("Guessed letters: {}".format(guessedLetterList))
+            print(*linesForPrinting)
 
+            playerLetterInput = input("Enter letter: ")
+            guessedLetterList.append(playerLetterInput)
+            x = 0
+
+            for slaveVariable in guessingWord:
+
+                if slaveVariable == playerLetterInput:
+                    linesForPrinting[x] = slaveVariable
+                    x += 1
+
+                elif slaveVariable != playerLetterInput:
+                    x += 1
+
+                elif x == len(linesForPrinting) - 1:
+                    cycleCounter += 1
+                    break
+
+    elif int(playerOption) == 2:
+        playerAddWord = input("Enter new word: ")
+        wordList.append(playerAddWord)
+        print("Added "+playerAddWord)
+
+    elif int(playerOption) == 3:
+        break
+
+    else:
+        print("Wrong input!")
