87c57f689decc9cfe1d78624ce44607a2ff78103
Agris <agrisskesters@gmail.com>
2021-10-22 18:11:00 +0300

    Home work 5

---
+++ homework_20.10.2021.py
@@ -0,0 +1,29 @@
+import random
+
+def random_number_for_account ():
+    emtyList = []
+    i = 0
+
+    while i < 21:
+        emtyList.append(random.randrange(1,9))
+        i +=1
+    else:
+        randomNumber = "".join((str(e) for e in emtyList))
+
+    return randomNumber
+
+
+class Account:
+
+    def __init__(self, number, balance):
+
+        self.number = number
+        self.balance = balance
+
+        self.number = random_number_for_account()
+
+    def add_money (amount):
+        if amount >= 0:
+
+
+
